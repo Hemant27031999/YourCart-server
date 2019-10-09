2f2f8edcd991f6c795e5d2af3083d560d0f397e2
Kunal Mohan <kunalmohan99@gmail.com>
2019-10-09 23:44:01 +0530

    database sync with cluster

--- my_tech_start/base_tech/cluster.py
+++ my_tech_start/base_tech/cluster.py
@@ -8,35 +8,71 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler 
 from sklearn.preprocessing import normalize 
 from sklearn.decomposition import PCA
+from .models import Vendors, Cells
 
-colnames = ['Lattitude', 'Longitude']
+def clusterer():
 
-X = pd.read_csv('./../../../../../dataset/melb_data1.csv', names=colnames)
+	colnames = ['Lattitude', 'Longitude']
+	
+	X = pd.read_csv('./../../../../../dataset/melb_data1.csv', names=colnames)
+	
+	print(X.head())
+	
+	Y = X[['Lattitude', 'Longitude']].values
+	
+	def distance(x, y):
+		lat1, long1 = x[0], x[1]
+		lat2, long2 = y[0], y[1]
+		return geodesic((lat1, long1), (lat2, long2)).meters
+	
+	eps0 = 200
+	min_vendors = 1
+	
+	est = DBSCAN(eps=eps0, min_samples=min_vendors, metric=distance).fit(Y)
+	X['cluster'] = est.labels_.tolist()
+	labels = est.labels_
+	
+	return labels
+	
+	#colors = pd.tools.plotting._get_standard_colors(len(labels), color_type='random')
+	
+	#plt.figure(figsize =(9, 9)) 
+	#plt.scatter(Y['Lattitude'], Y['Longitude'])
+	# Building the legend 
+	#plt.legend((r, g, b, k), ('Label 0', 'Label 1', 'Label 2', 'Label -1'))  
+	#plt.show() 
 
-print(X.head())
-
-Y = X[['Lattitude', 'Longitude']].values
-
-def distance(x, y):
-	lat1, long1 = x[0], x[1]
-	lat2, long2 = y[0], y[1]
-	return geodesic((lat1, long1), (lat2, long2)).meters
-
-eps0 = 500
-min_vendors = 1
-
-est = DBSCAN(eps=eps0, min_samples=min_vendors, metric=distance).fit(Y)
-X['cluster'] = est.labels_.tolist()
-labels = est.labels_
-
-print(labels)
-
-#colors = pd.tools.plotting._get_standard_colors(len(labels), color_type='random')
-
-plt.figure(figsize =(9, 9)) 
-plt.scatter(Y['Lattitude'], Y['Longitude']) 
+def unique(list1): 
   
-# Building the legend 
-plt.legend((r, g, b, k), ('Label 0', 'Label 1', 'Label 2', 'Label -1')) 
-  
-plt.show() 
+    unique_list = [] 
+    for x in list1: 
+        if x not in unique_list: 
+            unique_list.append(x)
+    return unique_list
+
+a = clusterer()
+print(a)
+
+b = unique(a)
+b.sort()
+
+n1 = len(a)
+n2 = len(b)
+
+vendors = Vendors.objects.all()
+
+for i in range(n1):
+	vendors(i).cell_no = a[i]
+
+Cells.objects.all().delete()
+
+objs = []
+
+for i in range(n2):
+	ven = Vendors.objects.filter(cell_no = b[i])
+	no = count(ven)
+	obj = Cells(cell_no=b[i], cell_lat=ven[0].latitude, cell_long=ven[0].longitude, no_vendors=no)
+	objs.append(obj)
+
+Cells.objects.bulk_create(objs, n2)
+
