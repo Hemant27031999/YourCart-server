f7e86ee3322421fdaf085a5dd49e9fd0ae1dafae
Kunal Mohan <kunalmohan99@gmail.com>
2019-10-09 22:00:57 +0530

    tables added to admin

--- my_tech_start/base_tech/admin.py
+++ my_tech_start/base_tech/admin.py
@@ -4,3 +4,9 @@ from base_tech.models import *
 # Register your models here.
 admin.site.register(Orders)
 admin.site.register(RegUser)
+admin.site.register(Category)
+admin.site.register(CategorizedProducts)
+admin.site.register(Addresses)
+admin.site.register(indep_Addresses)
+admin.site.register(Vendors)
+admin.site.register(Vendor_Products)
